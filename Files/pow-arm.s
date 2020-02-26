70753618b7ff700099473efdaac1510df130cdf2
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2020-02-26 00:29:14 +0000

    ready to compile

--- Files/pow-arm.s
+++ Files/pow-arm.s
@@ -1,49 +1,37 @@
-	.syntax unified
-	.cpu	arm7tdmi
-	.eabi_attribute	6, 2
-	.eabi_attribute	8, 1
-	.eabi_attribute	20, 1
-	.eabi_attribute	21, 1
-	.eabi_attribute	23, 3
-	.eabi_attribute	24, 1
-	.eabi_attribute	25, 1
-	.file	"pow.c"
 	.text
+	.file	"pow.c"
 	.globl	power
 	.align	2
-	.type	power,%function
+	.type	power,@function
 power:
-	push	{r11, lr}
-	mov	r11, sp
-	sub	sp, sp, #24
-	str	r0, [r11, #-8]
-	str	r1, [sp, #12]
-	mov	r2, r1
-	cmp	r1, #0
-	str	r0, [sp, #8]
-	str	r2, [sp, #4]
-	bne	.LBB0_2
-	b	.LBB0_1
-.LBB0_1:
-	mov	r0, #1
-	str	r0, [r11, #-4]
+	stp	x29, x30, [sp, #-16]!
+	mov	 x29, sp
+	sub	sp, sp, #32
+	str	x0, [sp, #16]
+	str	w1, [sp, #12]
+	ldr	w1, [sp, #12]
+	cbnz	w1, .LBB0_2
+	orr	x8, xzr, #0x1
+	stur	x8, [x29, #-8]
 	b	.LBB0_3
 .LBB0_2:
-	ldr	r0, [r11, #-8]
-	ldr	r1, [sp, #12]
-	sub	r1, r1, #1
-	str	r0, [sp]
+	ldr	x8, [sp, #16]
+	ldr	x0, [sp, #16]
+	ldr	w9, [sp, #12]
+	sub	w1, w9, #1
+	str		x8, [sp]
 	bl	power
-	ldr	r2, [sp]
-	mul	r1, r2, r0
-	str	r1, [r11, #-4]
+	ldr		x8, [sp]
+	mul		x8, x8, x0
+	stur	x8, [x29, #-8]
 .LBB0_3:
-	ldr	r0, [r11, #-4]
-	mov	sp, r11
-	pop	{r11, lr}
-	bx	lr
-.Ltmp0:
-	.size	power, .Ltmp0-power
+	ldur	x0, [x29, #-8]
+	mov	 sp, x29
+	ldp	x29, x30, [sp], #16
+	ret
+.Lfunc_end0:
+	.size	power, .Lfunc_end0-power
 
 
-	.ident	"clang version 3.4.2 (tags/RELEASE_34/dot2-final)"
+	.ident	"clang version 3.8.0-2ubuntu4 (tags/RELEASE_380/final)"
+	.section	".note.GNU-stack","",@progbits
